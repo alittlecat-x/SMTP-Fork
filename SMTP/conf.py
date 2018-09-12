9b7569a74e96dc2161eecabe5cdb8cc908a1a71c
ALittleCat <1005678141@qq.com>
2018-09-13 00:08:10 +0800

    Add files via upload

---
+++ SMTP/conf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+ALi
+20180911
+Python 3.7.0
+"""
+
+
+# text图像化参数
+round_image = True  # 是否开启text图像化
+font_path = 'SMTP/font/simhei.ttf'  # 字库路径
+word_size_num = 24  # 字号
+image_path = 'SMTP/file/image.jpg'  # 转换后图片保存位置
+
+# 附件路径
+file_path = 'SMTP/file/file'
